ecdffd87a6ba651172ca0de72d5c104df7e5c9b5
Umut Altun <umut390umut@gmail.com>
2024-01-03 02:08:43 +0300

    mc

---
+++ Section5/FindMax.asm
@@ -0,0 +1,32 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+lea si, a 
+mov di, si
+inc di 
+mov cl, 6
+
+mov al, [si] 
+    
+comp:   
+    mov bl, [di]
+    cmp bl, al
+    ja look
+    inc di 
+    loop comp
+jmp end 
+    
+look:
+    mov al, [di]
+    inc di
+    loop comp
+        
+end:
+ret
+
+a db 1,2,4,2,3,7,6
+
+
